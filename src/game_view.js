2c3cb7bbc09cdd91f5afc090667d226fa0a39068
Yuci Shen <36720898+yshen901@users.noreply.github.com>
2019-11-27 15:08:57 -0800

    Centralize settings params and setup building requirements

--- src/game_view.js
+++ src/game_view.js
@@ -1,57 +1,17 @@
 import Map from './map';
 import BuildingsList from './buildings_list';
 
+import { mapSize, resources, buildings, buildingParams } from './settings';
+
 class GameView {
   constructor(ctx, canvasEl) {
     this.ctx = ctx;
     this.canvasEl = canvasEl;
 
-    this.mapSize = {
-      X: 60,
-      Y: 60,
-      WIDTH: 1200,
-      HEIGHT: 640,
-      SQUARE_SIZE: 50
-    };
-
-    this.buildingParams = {
-      Y: this.mapSize.Y + this.mapSize.HEIGHT + 10,
-      X: this.mapSize.X + 20,
-      BUTTON_SIZE: 40,
-      SYMBOLS: ["HQ", "AC", "BC"],
-    };
-
-    this.resources = {
-      A: {
-        SYMBOL: "A",
-        AMOUNT: 5,
-      },
-      B: {
-        SYMBOL: "B",
-        AMOUNT: 5,
-      }
-    }
-
-    this.buildings = {
-      HQ: {
-        SYMBOL: "HQ",
-        REQUIRE: null,
-      },
-      BC: {
-        SYMBOL: "BC",
-        REQUIRE: {
-          HQ: "EXIST",
-          B: "ADJACENT",
-        }
-      },
-      AC: {
-        SYMBOL: "AC",
-        REQUIRE: {
-          HQ: "EXIST", 
-          A: "ADJACENT",
-        }
-      }
-    }
+    this.mapSize = mapSize;
+    this.resources = resources;
+    this.buildings = buildings;
+    this.buildingParams = buildingParams;
 
     this.building = null;
 
--- src/map.js
+++ src/map.js
@@ -96,7 +96,7 @@ class Map {
     }
   }
 
-  canPlace() {
+  canPlace(buildingSymbol, ) {
 
   }
 
